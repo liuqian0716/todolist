5ee6615feca2fba3157e36cb909d606a6e31a079
liuqian <1215381389@qq.com>
2018-12-26 23:03:06 +0800

    删除功能，htmlFor,dangerouslySetInnerHTML,className，注释

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './style.css'
 
 class Todolist extends Component {
   constructor(props){
     super(props);
     this.state = {
-      inputValue: 'hello',
+      inputValue: 'hello123',
       list: ['学习React','学习英语']
     }
   }
@@ -13,7 +14,13 @@ class Todolist extends Component {
     return (
       <div>
         <div>
+          {
+            //添加备注
+          }
+          <label htmlFor="inputBox">输入内容：</label>
           <input
+            id="inputBox"
+            className="input"
             value={this.state.inputValue} 
             onChange={this.handlerInput.bind(this)}
           />
@@ -21,8 +28,13 @@ class Todolist extends Component {
         </div>
         <ul>
           {
-            this.state.list.map(n=>{
-              return <li key={n}>{n}</li>
+            this.state.list.map((n,index)=>{
+              return <li
+                      key={n}
+                      dangerouslySetInnerHTML={{__html: n}}
+                      onClick={this.handlerDelete.bind(this,index)} 
+                      >
+                      </li>
             })
           }
         </ul>
@@ -38,6 +50,15 @@ class Todolist extends Component {
     this.setState({
       list: [...this.state.list,this.state.inputValue],
       inputValue: ''
+    })
+  }
+  handlerDelete (index) {
+    //immutable
+    // state 不允许我们做任何的改变（直接修改state）
+    const list = [...this.state.list];
+    list.splice(index,1);
+    this.setState({
+      list: list
     })
   }
 }
